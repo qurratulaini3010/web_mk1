bd072f11d011be0b174eb58bfac3db220ee72fc9
qurratulaini3010 <missquratulaini@gmail.com>
2023-09-04 11:59:01 +0800

    Qurratul

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Qurratul</title>
-</head>
-<body>
-<h1>
-    Reverse The Name
-</h1>
 <?php
-$nama = "qurratul";
+echo 'REVERSE NAME ';
+$nama = " qurratul ";
 echo $nama;
-echo "<br>";
 echo strrev($nama);
 ?>
-</body>
-</html>
